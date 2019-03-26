73dded7f176f1a6d76437abf73b71ab902b1aab0
Salikov Roman <develop51@datum-group.ru>
2019-03-26 18:06:16 +0300

    Changed database engine to Postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'supersecret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
