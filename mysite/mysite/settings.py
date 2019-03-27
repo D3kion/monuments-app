4b00cf88f954f44f064c3a11dee0cb91758f4213
Salikov Roman <develop51@datum-group.ru>
2019-03-27 16:27:12 +0300

    Updated permissions

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,7 +131,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
