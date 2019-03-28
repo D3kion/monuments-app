e3d3d6f8d778bb2feaad9fb5c687842b766e9a5f
Salikov Roman <develop51@datum-group.ru>
2019-03-28 10:58:17 +0300

    Some changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,17 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM'
 
+const layer = new TileLayer({
+  source: new OSM(),
+});
+
+const view = new View({
+  center: [0, 0],
+  zoom: 3,
+});
+
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 5,
-  }),
+  layers: [layer],
+  view: view,
 });
