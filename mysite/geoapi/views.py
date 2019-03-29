58ee85b55c3cd4a06bcfff2a08b80453a0db1382
D3kion <dekion@protonmail.com>
2019-03-29 16:56:55 +0300

    Turned off pagination in geoapi

--- mysite/geoapi/views.py
+++ mysite/geoapi/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework_gis.pagination import GeoJsonPagination
 
 from .models import City, Country
 from .serializers import CitySerializer, CountrySerializer
@@ -8,10 +7,10 @@ from .serializers import CitySerializer, CountrySerializer
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all().order_by('name')
     serializer_class = CountrySerializer
-    pagination_class = GeoJsonPagination
+    pagination_class = None
 
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all().order_by('name')
     serializer_class = CitySerializer
-    pagination_class = GeoJsonPagination
+    pagination_class = None
