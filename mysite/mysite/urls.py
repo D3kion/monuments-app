360db409679f4447b056df052d7cfa6ca8aef4c5
D3kion <dekion@protonmail.com>
2019-03-29 17:22:17 +0300

    Changed url to polls app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('api/', include('api.urls')),
     path('geoapi/', include('geoapi.urls')),
     path('admin/', admin.site.urls),
