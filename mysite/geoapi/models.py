c9e2b4a5b30a2ecb2591314586283475c56933ed
D3kion <dekion@protonmail.com>
2019-03-29 12:10:55 +0300

    Added plural names to models

--- mysite/geoapi/models.py
+++ mysite/geoapi/models.py
@@ -8,18 +8,26 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'country'
+        verbose_name_plural = 'countries'
+
 
 class City(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     # photos
     country = models.ForeignKey(Country, on_delete=models.DO_NOTHING,
-                                null=True)  # Need review
+                                null=True)
     geometry = models.PointField()
 
     def __str__(self):
         return self.name
 
-# Need review
+    class Meta:
+        verbose_name = 'city'
+        verbose_name_plural = 'cities'
+
+
 # class Capital(City):
 #     capital_of = models.OneToOneField(Country, on_delete=models.DO_NOTHING)
