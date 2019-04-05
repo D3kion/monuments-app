f04de19d3534a04be00d0324415ff7bce59d7d9d
D3kion <dekion@protonmail.com>
2019-04-05 09:50:11 +0300

    Updated urls for images

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('api/geo/', include('geoapi.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
