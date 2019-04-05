656efdc540236b9d136b32c720d0e2ce69a8704e
D3kion <dekion@protonmail.com>
2019-04-05 09:49:26 +0300

    Added some models to admin

--- django/geoapi/admin.py
+++ django/geoapi/admin.py
@@ -1,6 +1,8 @@
 from django.contrib.gis import admin
 
-from .models import Country, City
+from .models import Country, City, Capital, Image
 
 admin.site.register(Country, admin.GeoModelAdmin)
 admin.site.register(City, admin.GeoModelAdmin)
+admin.site.register(Capital)
+admin.site.register(Image)
