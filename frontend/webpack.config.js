71a5e396a4209afe4e0bd171460a5cda8ff1979f
D3kion <dekion@protonmail.com>
2019-04-08 11:41:31 +0300

    Added font-awesome

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,7 +30,15 @@ module.exports = {
       {
         test: /\.hbs$/,
         loader: 'handlebars-loader',
-      }
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
     ],
   },
 
