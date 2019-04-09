921de795c91013903fba1c11cfb23db833125ff9
D3kion <dekion@protonmail.com>
2019-04-09 10:13:06 +0300

    Some refactor

--- backend/geoapi/urls.py
+++ backend/geoapi/urls.py
@@ -10,17 +10,20 @@ router.register('capital', views.CapitalViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Country
     path('geojson/country/', views.CountryGeoView.as_view(),
          name='country-geo'),
-    path('geojson/city/', views.CityGeoView.as_view(), name='city-geo'),
     path('info/country/<int:pk>/', views.CountryInfoView.as_view(),
          name='country-info'),
-    path('info/city/<int:pk>/', views.CityInfoView.as_view(),
-         name='city-info'),
     path('search/country/', views.CountrySearchView.as_view(),
          name='country-search'),
+    # City
+    path('geojson/city/', views.CityGeoView.as_view(), name='city-geo'),
+    path('info/city/<int:pk>/', views.CityInfoView.as_view(),
+         name='city-info'),
     path('search/city/', views.CitySearchView.as_view(),
          name='city-search'),
+    # CountriesHelper
     path('countries/', views.CountriesHelperListView.as_view(),
          name='countires-list'),
     path('countries/<int:pk>/', views.CountriesHelperView.as_view(),
