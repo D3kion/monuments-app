b1404ef4ca4ba9092f0124ac556005027878feee
D3kion <dekion@protonmail.com>
2019-04-09 14:38:59 +0300

    Some refactror

--- frontend/src/app/main/menu/create.js
+++ frontend/src/app/main/menu/create.js
@@ -18,61 +18,63 @@ export default View.extend({
 
   events: {
     'click @ui.choose': 'onChoose',
-    'click @ui.country': 'onCountrySelect',
     'click @ui.submit': 'onSubmit',
   },
 
   initialize() {
-    this.model.on('change:countries', this.render, this)
-    this.model.on('change:isCountry', this.render, this)
+    this.model.on('change', this.render, this)
     this.loadCountries()
   },
 
   onChoose(e) {
-    if (e.target.dataset.choose == 'country')
-      this.model.set('isCountry', true)
-    else
-      this.model.set('isCountry', false)
-  },
+    this.model.set('isCountry', false)
+    this.model.set('isCity', false)
+    this.model.set('isCapital', false)
 
-  onCountrySelect(e) {
-    this.model.set('countryName', e.target.value)
-    this.model.set('countryId', e.target.children[e.target.selectedIndex].dataset.id)
+    switch (e.target.dataset.choose) {
+      case 'country':
+        this.model.set('isCountry', true)
+        break;
+      case 'city':
+        this.model.set('isCity', true)
+        break;
+      case 'capital':
+        this.model.set('isCapital', true)
+        break;
+    }
   },
 
   onSubmit() {
-    const url = 'api/geo/countries/' + this.model.get('countryId') + '/'
+    if (this.model.get('isCountry'))
+      this.onSubmitCountry()
+
+  },
+
+  onSubmitCountry() {
+    const id = this.getUI('country').val()
+    const itemId = this.getUI('country').prop('selectedIndex')
+    const name = this.getUI('country').prop('options').item(itemId).text
+
+    const url = 'api/geo/countries/' + id + '/'
     fetch('GET', url)
     .then(res => res.json())
     .then(data => {
-      this.model.set('countryGeometry', data.geometry)
-
       fetch('POST', 'api/geo/country/', JSON.stringify({
-        name: this.model.get('countryName'),
-        geometry: this.model.get('countryGeometry'),
+        name,
+        geometry: data.geometry,
       }))
-      .then(res => res.json())
-      .then(data => {
-        if (typeof data.id !== 'undefined') {
+      .then(res => {
+        if (res.ok) {
           this.triggerMethod('refresh:map', this)
           this.loadCountries()
         }
-        else
-          console.log('Ошибка: ', data)
       })
     })
-
   },
 
   loadCountries() {
     fetch('GET', 'api/geo/countries/')
     .then(res => res.json())
-    .then(data => {
-      this.model.set('countries', data)
-      if (data.length > 0) {
-        this.model.set('countryName', data[0].name)
-        this.model.set('countryId', data[0].id)
-      }
-    })
+    .then(data => this.model.set('countries', data))
   },
 })
