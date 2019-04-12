c833c7a13c3cfaf5d811a36903876e7748881ab7
D3kion <dekion@protonmail.com>
2019-04-12 17:35:50 +0300

    Implemented adding images on creation

--- frontend/src/app/views/main/menu/create/city.js
+++ frontend/src/app/views/main/menu/create/city.js
@@ -1,5 +1,6 @@
 import { View } from 'backbone.marionette'
 import template from './city.hbs'
+import fetch from '../../../../utils'
 import CityModel from 'Models/city'
 import CountriesCollection from 'Collections/countries'
 
@@ -7,6 +8,7 @@ export default View.extend({
   template: template,
 
   events: {
+    'change #images': 'onChangeImages',
     'click #place': 'onPlace',
     'click #submit': 'onSubmit',
   },
@@ -27,8 +29,12 @@ export default View.extend({
     }
   },
 
+  onChangeImages(e) {
+    this.images = e.target.files
+  },
+
   onPlace() {
-    this.drawPoint((coords) => this.city.set({geometry: {type: 'Point', coordinates: coords}}))
+    this.drawPoint(coords => this.city.set({geometry: {type: 'Point', coordinates: coords}}))
   },
 
   onSubmit() {
@@ -41,7 +47,20 @@ export default View.extend({
       country_: data.country,
       description: data.description,
     }, {
-      success: () => {
+      success: (model) => {
+        if (typeof this.images !== 'undefined')
+          for (let i = 0; i < this.images.length; i++) {
+            let formData = new FormData()
+            formData.append('city', model.get('id'))
+            formData.append('image', this.images[i])
+      
+            fetch('POST', 'api/image/', formData)
+            .then(res => {
+              if (!res.ok)
+                console.log(res)
+            })
+          }
+
         this.triggerMethod('refresh:map', this)
         this.triggerMethod('close:menu', this)
       },
