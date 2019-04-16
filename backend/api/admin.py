1aa549f3fe9a7b18d0214d68f4230201bdce746f
D3kion <dekion@protonmail.com>
2019-04-16 09:28:03 +0300

    #21

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib.gis import admin
+from django.contrib.auth.models import Group
 
 from .models import Country, City, Capital, Image
+
+admin.site.unregister(Group)
 
 admin.site.register(Country, admin.GeoModelAdmin)
 admin.site.register(City, admin.GeoModelAdmin)
