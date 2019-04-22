0bf07bf382eed2e066258820729da1f91b961d77
D3kion <dekion@protonmail.com>
2019-04-22 15:00:04 +0300

    Added example of configuration of smtp server using gmail

--- backend/mysite/local_settings.example.py
+++ backend/mysite/local_settings.example.py
@@ -8,3 +8,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'USER@gmail.com'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
+EMAIL_USE_TLS = True
