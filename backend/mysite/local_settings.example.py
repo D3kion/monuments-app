6f61519fa5a4c0e0118147adc9fa2a7c2d5e1cbd
D3kion <dekion@protonmail.com>
2019-04-23 10:25:24 +0300

    update

--- backend/mysite/local_settings.example.py
+++ backend/mysite/local_settings.example.py
@@ -2,20 +2,9 @@ DEBUG = True
 
 SECRET_KEY = 'jstdyvlpcssdo4#2=atx!d=n+qfg#4k=96&yq_%(kay3flwczq'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DBNAME',
-        'USER': 'DBUSER',
-        'PASSWORD': 'DBPASSWORD',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'USER@gmail.com'
-EMAIL_HOST_PASSWORD = 'PASSWORD'
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'USER@gmail.com'
+# EMAIL_HOST_PASSWORD = 'PASSWORD'
+# EMAIL_USE_TLS = True
