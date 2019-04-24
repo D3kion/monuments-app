560aa522b641f551212c0b51b09e2e4165cdfa2a
D3kion <dekion@protonmail.com>
2019-04-24 16:21:28 +0300

    Added country to list display of Capital and City

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,11 +1,21 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib.gis import admin
 from django.contrib.auth.models import Group
 
 from .models import Country, City, Capital, Image
 
+
+class CityAdmin(admin.GeoModelAdmin):
+    list_display = ['name', 'country']
+
+
+class CapitalAdmin(ModelAdmin):
+    list_display = ['city', 'capital_of']
+
+
 admin.site.unregister(Group)
 
 admin.site.register(Country, admin.GeoModelAdmin)
-admin.site.register(City, admin.GeoModelAdmin)
-admin.site.register(Capital)
+admin.site.register(City, CityAdmin)
+admin.site.register(Capital, CapitalAdmin)
 admin.site.register(Image)
