0658b0170906108dc32a3f8e31fde7172ee4c6a3
D3kion <dekion@protonmail.com>
2019-05-23 12:55:51 +0300

    Removed groups and permissions

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -10,8 +10,8 @@ class MyUserAdmin(UserAdmin):
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name',
                                          'patronymic', 'email', 'job')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff',
+                                       'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     list_display = ('username', 'email', 'first_name', 'last_name',
